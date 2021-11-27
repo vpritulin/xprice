cefdb19ecc1fde7757cb2970b0cfda42361c689e
Vladislav <v.pritulin@gmail.com>
2021-11-27 17:56:27 +0200

    update parse

--- index.js
+++ index.js
@@ -1,68 +1,83 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer')
 
-let link = 'https://www.foxtrot.com.ua/ru/shop/mobilnye_telefony_smartfon.html?page';
+let link = 'https://www.foxtrot.com.ua/ru/shop/mobilnye_telefony_smartfon.html?page=';
 
-(async ()=>{
+
+
+(async () => {
     let flag = true;
     let res = [];
     let counter = 1;
 
     try {
+
         let browser = await puppeteer.launch({
-            headless:false,
-            slowMo:100,
-            devtools:true
+            headless: false,
+            slowMo: 100,
+            devtools: true
         })
         let page = await browser.newPage()
         await page.setViewport({
-            width:1400,
-            height:900
+            width: 1400,
+            height: 900
         })
-        while (flag){
+        while (flag) {
             await page.goto(`${link}${counter}`)
             await page.waitForSelector('li.listing__pagination-nav')
             console.log(counter)
 
-            let html = await page.evaluate(async ()=>{
+            let html = await page.evaluate(async () => {
                 let page = [];
                 try {
                     let divs = document.querySelectorAll('div.card')
-                    divs.forEach(div=>{
+                    divs.forEach(div => {
                         let a = div.querySelector('a.card__title')
-                        let obj ={
-                            title: a !=null
+                        let discount = div.querySelector("div.card__price-discount").innerText;
+
+                        function split(str) {
+                            var i = str.indexOf("\n\n");
+                            if (i > 0)
+                                return str.slice(0, i)
+                        }
+
+                        let obj = {
+                            title: a != null
                                 ? a.innerText
                                 : 'No-Link',
-                            link:a.href,
-                            price:div.querySelector("div.card-price").innerText != null
+                            link: a.href,
+                            discount: split(discount),
+                            price: div.querySelector("div.card-price").innerText != null
                                 ? div.querySelector("div.card-price").innerText
                                 : 'No-Price'
                         }
-
                         page.push(obj)
                     })
-                }catch (e){
+                } catch (e) {
                     console.log(e)
                 }
                 return page;
-            },{waitUntil:'a.listing__pagination-nav'})
+            }, {waitUntil: 'a.listing__pagination-nav'})
+
             await res.push(html)
-            for (let i in res){
+
+            for (let i in res) {
                 if (res[i].length === 0) flag = false
             }
             counter++
         }
         await browser.close();
         res = res.flat()
-
-        fs.write('foxtrot.json',JSON.stringify({'data':res}),error=>{
+        console.log(res)
+        fs.writeFile('foxtrot.json', JSON.stringify({'data': res}), err => {
             if (err) throw err
             console.log('foxtrot.json saved')
-            console.log('foxtrot.json length - ',res.length)
+            console.log('foxtrot.json length - ', res.length)
         });
-    }catch (e){
+    } catch (e) {
         console.log(e)
         await browser.close()
     }
-})().catch( e => { console.error(e) });
+})().catch(e => {
+    console.error(e)
+});
