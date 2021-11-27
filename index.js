8039aaaa04516cc25276780148e058c2ca6601ac
Vladislav <v.pritulin@gmail.com>
2021-11-27 10:07:36 +0200

    update parse

--- index.js
+++ index.js
@@ -31,11 +31,15 @@ let link = 'https://www.foxtrot.com.ua/ru/shop/mobilnye_telefony_smartfon.html?p
                     divs.forEach(div=>{
                         let a = div.querySelector('a.card__title')
                         let obj ={
-                            title: a.innerText,
+                            title: a !=null
+                                ? a.innerText
+                                : 'No-Link',
                             link:a.href,
-                            discount:div.querySelector('div.card__price-discount').innerText,
-                            price:div.querySelector("div.card-price").innerText
+                            price:div.querySelector("div.card-price").innerText != null
+                                ? div.querySelector("div.card-price").innerText
+                                : 'No-Price'
                         }
+
                         page.push(obj)
                     })
                 }catch (e){
@@ -44,10 +48,19 @@ let link = 'https://www.foxtrot.com.ua/ru/shop/mobilnye_telefony_smartfon.html?p
                 return page;
             },{waitUntil:'a.listing__pagination-nav'})
             await res.push(html)
-            console.log(res)
+            for (let i in res){
+                if (res[i].length === 0) flag = false
+            }
             counter++
         }
+        await browser.close();
+        res = res.flat()
 
+        fs.write('foxtrot.json',JSON.stringify({'data':res}),error=>{
+            if (err) throw err
+            console.log('foxtrot.json saved')
+            console.log('foxtrot.json length - ',res.length)
+        });
     }catch (e){
         console.log(e)
         await browser.close()
